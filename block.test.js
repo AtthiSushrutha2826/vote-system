7ecfb500a05ff8c0dad2c8e8d59638fffb2de753
JRBros2346 <jrbros2346@gmail.com>
2024-09-17 01:12:51 +0530

    [FIX] Block.js

--- block.test.js
+++ block.test.js
@@ -1,6 +1,6 @@
 const Block=require('./block');
 const {GENESIS_DATA}= require('./config');
-const cryptHash=require('./crypto-hash')
+const cryptoHash=require('./crypto-hash')
 
 describe('Block', () => {
 
@@ -57,8 +57,7 @@ describe('Block', () => {
             expect(minedBlock.timestamp).not.toEqual(undefined);
         });
         it('creates a SHA-256 `hash` based on the proper input', ()=>{
-             expect(minedBlock.hash)
-               .toEqual(cryptHash(minedBlock.timestamp, lastBlock.hash, data));
+             expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
         });
 
     });
